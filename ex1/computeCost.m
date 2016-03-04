ea1475566cf92aa14998a57863e0474b9fc17951
jarturomora <jarturomora@gmail.com>
2016-03-04 05:54:11 -0600

    computeCost function done

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% The cost function
 
-
-
+J = (1/(2*m)) * sum((X * theta - y).^2);
 
 % =========================================================================
 
